26a5a40d437e3e2617a031b02079cbddd30162cd
velishra <Rajeshkumar.velishoju@adp.com>
2016-04-05 14:35:18 +0530

    U2

--- Sample/src/simple/sample.java
+++ Sample/src/simple/sample.java
@@ -2,9 +2,9 @@ package simple;
 
 public class sample {
 public static void main1(String[] args) {
-	
+	System.out.println("U2");
 }
 public static void main(String[] args) {
-	
+	System.out.println("U2");
 }
 }
