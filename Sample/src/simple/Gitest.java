5cd1c3da4e0de8258e0f70e4c85a6d1ebfd5c88a
velishra <Rajeshkumar.velishoju@adp.com>
2016-04-05 11:44:45 +0530

    merge issue resolved

--- Sample/src/simple/Gitest.java
+++ Sample/src/simple/Gitest.java
@@ -7,8 +7,8 @@ public class Gitest {
 		System.out.println("Added in main by u1");
 		System.out.println("Added in main by U2");
 		
-		System.out.println("added by u2-----------");
 		System.out.println("added by u1-----------");
+		System.out.println("added by u2-----------");
 	}
 	
 	private void u1() {
